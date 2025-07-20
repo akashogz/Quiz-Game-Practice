6ef3ecbe457cee0419e5c0fe1e499f9b3ec05e41
Akash Gautam <akashogz@gmail.com>
2025-07-20 13:27:22 +0530

    Initial commit

---
+++ quiz game/quiz.js
@@ -0,0 +1,97 @@
+const quizQuestions = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hyper Text Markup Language",
+      "Hot Mail",
+      "How To Make Links",
+      "Hyperlinks and Text Markup Language"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which tag is used to create a hyperlink in HTML?",
+    options: ["<link>", "<a>", "<href>", "<url>"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which property is used to change the background color in CSS?",
+    options: ["color", "bgcolor", "background-color", "background"],
+    correctAnswer: 2
+  },
+  {
+    question: "How do you write a comment in CSS?",
+    options: ["// comment", "/* comment */", "# comment", "<!-- comment -->"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the correct syntax to link a CSS file to HTML?",
+    options: [
+      "<style src='style.css'>",
+      "<link rel='stylesheet' href='style.css'>",
+      "<css href='style.css'>",
+      "<script src='style.css'>"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: "Inside which tag do we write JavaScript?",
+    options: ["<js>", "<scripting>", "<script>", "<javascript>"],
+    correctAnswer: 2
+  },
+  {
+    question: "Which method is used to write content into the browser in JS?",
+    options: ["console.log()", "alert()", "document.write()", "window.alert()"],
+    correctAnswer: 2
+  },
+  {
+    question: "How do you declare a variable in JavaScript?",
+    options: ["var x;", "let x;", "const x;", "All of the above"],
+    correctAnswer: 3
+  },
+  {
+    question: "What will `typeof []` return in JavaScript?",
+    options: ["object", "array", "list", "undefined"],
+    correctAnswer: 0
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    options: ["<!-- -->", "//", "/* */", "#"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which CSS property controls the text size?",
+    options: ["font-style", "text-size", "font-size", "text-style"],
+    correctAnswer: 2
+  },
+  {
+    question: "How do you center a block element horizontally in CSS?",
+    options: [
+      "margin: auto;",
+      "text-align: center;",
+      "align: center;",
+      "display: center;"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which tag is used to add an image in HTML?",
+    options: ["<img>", "<image>", "<src>", "<picture>"],
+    correctAnswer: 0
+  },
+  {
+    question: "Which of the following is not a JavaScript data type?",
+    options: ["Number", "Boolean", "Float", "Undefined"],
+    correctAnswer: 2
+  },
+  {
+    question: "What does DOM stand for?",
+    options: [
+      "Document Object Model",
+      "Data Object Management",
+      "Digital Ordinance Model",
+      "Document Oriented Method"
+    ],
+    correctAnswer: 0
+  }
+];
